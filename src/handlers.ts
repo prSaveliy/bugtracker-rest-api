1a2a97cf2aed6b677698b8c3618d6e0170688b7a
prisyaga <papsavel@gmail.com>
2026-01-29 20:23:35 +0200

    refactor: move request handlers to a separate module

---
+++ src/handlers.ts
@@ -0,0 +1,145 @@
+import { IncomingMessage } from "node:http";
+
+import { router, Server } from "./server.js";
+
+const urlRegExp: RegExp = /^\/bugs\/(\d+)$/; 
+
+router.add({
+  method: "GET",
+  url: urlRegExp,
+  handler: async (context: Server, id: string, req: IncomingMessage) => {
+  if (Object.hasOwn(context.bugs, +id)) {
+    return {
+      body: JSON.stringify(context.bugs[+id]),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+  } else {
+    return {
+      status: 404,
+      body: `Bug with id: ${+id} not found.`
+    };
+  }
+} });
+
+async function parseRequestJSON(req: IncomingMessage): Promise<any> {
+  return new Promise((resolve, reject) => {
+    let body = "";
+    req.on("data", chunk => body += chunk);
+    req.on("end", () => {
+      try {
+        resolve(JSON.parse(body));
+      } catch (err) {
+        reject(err);
+      }
+    })
+    req.on("error", err => reject(err));
+  })
+}
+
+router.add({
+  method: "PUT", 
+  url: urlRegExp, 
+  handler: async (context: Server, id: string, req: IncomingMessage) => {
+  let body: any;
+  parseRequestJSON(req)
+    .then(data => {body = data})
+    .catch(err => {
+      return {
+        status: 400,
+        body: "Invalid JSON."
+      }
+    });
+  if (
+    !body ||
+    typeof body.id !== 'number' ||
+    typeof body.title !== 'string' ||
+    typeof body.description !== 'string'
+  ) {
+    return {
+      status: 400,
+      body: "Bad bug data."
+    };
+  }
+  context.bugs[+id] = {
+    id: +id,
+    title: body.title,
+    description: body.description,
+    status: "open",
+    comments: []
+  };
+  context.updated();
+  return { status: 204 };
+} });
+
+router.add({
+  method: "DELETE", 
+  url: urlRegExp, 
+  handler: async (context: Server, id: string, req: IncomingMessage) => {
+  if (Object.hasOwn(context.bugs, +id)) {
+    delete context.bugs[+id];
+    context.updated();
+  }
+  return { status: 204 };
+} });
+
+router.add({ 
+  method: "POST", 
+  url: /^\/bugs\/(\d+)\/comments/, 
+  handler: async (context: Server, id: string, req: IncomingMessage) => {
+    let commentData: any;
+    parseRequestJSON(req)
+      .then(data => {commentData = data})
+      .catch(err => {
+        return {
+          status: 400,
+          body: "Invalid JSON."
+        }
+      })
+    if (
+      !commentData ||
+      commentData.author !== 'string' ||
+      commentData.message !== 'string'
+    ) {
+      return {
+        status: 400,
+        body: "Bad comment data."
+      }
+    }
+    if (Object.hasOwn(context.bugs, +id)) {
+      context.bugs[+id].comments.push(commentData);
+      if (context.bugs[+id].status === "open") {
+        context.bugs[+id].status = "in-progress";
+      }
+      context.updated();
+      return { status: 204 };
+    } else {
+      return {
+        status: 404,
+        body: `Bug with id: ${+id} not found.`
+      };
+    }
+} });
+
+router.add({ 
+  method: "GET", 
+  url: /^\/bugs$/, 
+  handler: async (context: Server, id: string, req: IncomingMessage) => {
+    let version;
+    let wait;
+    if (req.headers["if-none-match"]) {
+      version = /"(.*)"/.exec(req.headers["if-none-match"]);
+    }
+    if (req.headers["prefer"] && typeof req.headers["prefer"] === 'string') {
+      wait = /\bwait=(\d+)/.exec(req.headers["prefer"])
+    }
+
+    if (!version || +version[1] !== context.version) {
+      return context.getBugs();
+    } else if (!wait) {
+      return { status: 304 };
+    } else {
+      return context.waitForChanges(+wait[1]);
+    }
+} });
--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import type { PromiseResolveFunction, Bugs, ServeOptions, Headers, Response } fr
 
 import { Router } from "./router.js";
 
-const router = new Router();
+export const router = new Router();
 
 export class Server {
   public version: number;
@@ -56,126 +56,6 @@ async function serveFromRouter({ context, req, res, next }: ServeOptions) {
   }
 }
 
-const urlRegExp: RegExp = /^\/bugs\/(\d+)$/; 
-
-router.add({
-  method: "GET",
-  url: urlRegExp,
-  handler: async (context: Server, id: string, req: IncomingMessage) => {
-  if (Object.hasOwn(context.bugs, +id)) {
-    return {
-      body: JSON.stringify(context.bugs[+id]),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    };
-  } else {
-    return {
-      status: 404,
-      body: `Bug with id: ${+id} not found.`
-    };
-  }
-} });
-
-async function parseRequestJSON(req: IncomingMessage): Promise<any> {
-  return new Promise((resolve, reject) => {
-    let body = "";
-    req.on("data", chunk => body += chunk);
-    req.on("end", () => {
-      try {
-        resolve(JSON.parse(body));
-      } catch (err) {
-        reject(err);
-      }
-    })
-    req.on("error", err => reject(err));
-  })
-}
-
-router.add({
-  method: "PUT", 
-  url: urlRegExp, 
-  handler: async (context: Server, id: string, req: IncomingMessage) => {
-  let body: any;
-  parseRequestJSON(req)
-    .then(data => {body = data})
-    .catch(err => {
-      return {
-        status: 400,
-        body: "Invalid JSON."
-      }
-    });
-  if (
-    !body ||
-    typeof body.id !== 'number' ||
-    typeof body.title !== 'string' ||
-    typeof body.description !== 'string'
-  ) {
-    return {
-      status: 400,
-      body: "Bad bug data."
-    };
-  }
-  context.bugs[+id] = {
-    id: +id,
-    title: body.title,
-    description: body.description,
-    status: "open",
-    comments: []
-  };
-  context.updated();
-  return { status: 204 };
-} });
-
-router.add({
-  method: "DELETE", 
-  url: urlRegExp, 
-  handler: async (context: Server, id: string, req: IncomingMessage) => {
-  if (Object.hasOwn(context.bugs, +id)) {
-    delete context.bugs[+id];
-    context.updated();
-  }
-  return { status: 204 };
-} });
-
-router.add({ 
-  method: "POST", 
-  url: /^\/bugs\/(\d+)\/comments/, 
-  handler: async (context: Server, id: string, req: IncomingMessage) => {
-    let commentData: any;
-    parseRequestJSON(req)
-      .then(data => {commentData = data})
-      .catch(err => {
-        return {
-          status: 400,
-          body: "Invalid JSON."
-        }
-      })
-    if (
-      !commentData ||
-      commentData.author !== 'string' ||
-      commentData.message !== 'string'
-    ) {
-      return {
-        status: 400,
-        body: "Bad comment data."
-      }
-    }
-    if (Object.hasOwn(context.bugs, +id)) {
-      context.bugs[+id].comments.push(commentData);
-      if (context.bugs[+id].status === "open") {
-        context.bugs[+id].status = "in-progress";
-      }
-      context.updated();
-      return { status: 204 };
-    } else {
-      return {
-        status: 404,
-        body: `Bug with id: ${+id} not found.`
-      };
-    }
-} });
-
 Server.prototype.getBugs = function () {
   const bugs = Object.keys(this.bugs).map(id => this.bugs[+id]);
   return {
@@ -187,28 +67,6 @@ Server.prototype.getBugs = function () {
     }
   };
 };
-
-router.add({ 
-  method: "GET", 
-  url: /^\/bugs$/, 
-  handler: async (context: Server, id: string, req: IncomingMessage) => {
-    let version;
-    let wait;
-    if (req.headers["if-none-match"]) {
-      version = /"(.*)"/.exec(req.headers["if-none-match"]);
-    }
-    if (req.headers["prefer"] && typeof req.headers["prefer"] === 'string') {
-      wait = /\bwait=(\d+)/.exec(req.headers["prefer"])
-    }
-
-    if (!version || +version[1] !== context.version) {
-      return context.getBugs();
-    } else if (!wait) {
-      return { status: 304 };
-    } else {
-      return context.waitForChanges(+wait[1]);
-    }
-} });
 
 Server.prototype.waitForChanges = function(wait: number): Promise<any> {
   return new Promise(resolve => {
