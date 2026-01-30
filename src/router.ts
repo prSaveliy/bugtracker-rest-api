f429ad03465677e12bd757e8e2b204054fa0974e
prisyaga <papsavel@gmail.com>
2026-01-30 16:39:23 +0200

    feat: add protection from wrong methods

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,14 @@
 import { IncomingMessage } from "node:http";
 
 import type { Route } from "./types.js"
-
 import type { Server } from "./server.js";
+
+async function methodNotAllowed(req: IncomingMessage) {
+  return {
+    status: 405,
+    body: `Method ${req.method} not allowed.`
+  };
+}
 
 export class Router {
   public routes: Route[] = []
@@ -16,9 +22,13 @@ export class Router {
     const { pathname } = new URL(url, "http://d");
     for (let { method, url, handler } of this.routes) {
       const match = url.exec(pathname);
-      if (!match || method !== req.method) continue;
-      const urlItem = match[1];
-      return handler(context, urlItem, req)
+      if (!this.routes.some(route => route.method === req.method)) {
+        return methodNotAllowed(req);
+      } else if (!match || method !== req.method) continue;
+      else {
+        const urlItem = match[1];
+        return handler(context, urlItem, req);
+      }
     }
   }
 }
